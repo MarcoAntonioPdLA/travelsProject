0ed554d6cdaed3f99324f9d16a64bf467264a578
MarcoAntonioPonceDeLeon <mponcedeleona@unsa.edu.pe>
2021-06-11 08:10:12 -0500

    Cambiando enlace de travello

--- travels/urls.py
+++ travels/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('travello.urls'))
+    path('travello', include('travello.urls'))
 ]
