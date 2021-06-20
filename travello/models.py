a4eef853126ce6633f6d897376d9c72ac6f69f0f
MarcoAntonioPdLA <mponcedeleona@unsa.edu.pe>
2021-06-20 14:19:06 -0500

    Creando modelo Destination

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+  title = models.CharField(max_length = 100)
+  image = models.ImageField(upload_to = 'images')
+  subtitle = models.TextField()
+  price = models.IntegerField()
