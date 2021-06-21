a0057aa30a512cc2bd37a75b9c4a730b21da871f
MarcoAntonioPdLA <mponcedeleona@unsa.edu.pe>
2021-06-20 23:41:00 -0500

    Creando formulario para creación de usuarios

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def register():
-  pass
+def register(request):
+  return render(request, 'register.html')
--- travels/urls.py
+++ travels/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('travello/', include('travello.urls')),
+    path('', include('travello.urls')),
     path('accounts/', include('accounts.urls'))
 ]
 
