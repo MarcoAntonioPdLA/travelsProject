5ce8eb49a0034b371584685469a641e36bb631a9
MarcoAntonioPdLA <mponcedeleona@unsa.edu.pe>
2021-06-20 23:52:21 -0500

    Creando un usuario de prueba

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def register(request):
-  return render(request, 'register.html')
+  if request.method == 'POST':
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password_1 = request.POST['password_1']
+    password_2 = request.POST['password_2']
+
+    user = User.objects.create_user(first_name = first_name, last_name = last_name, username = username, email = email, password = password_1)
+    user.save()
+    print('Usuario creado')
+    return redirect('/')
+  else:
+    return render(request, 'register.html')
